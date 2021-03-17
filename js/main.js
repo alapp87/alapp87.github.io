83086f66f84b728cfbfc0868e440e3ad58877b6c
Anthony Lapp <anthonylapp87@yahoo.com>
2021-03-17 08:06:11 -0400

    additional experience and skills and style changes

--- js/main.js
+++ js/main.js
@@ -20,11 +20,11 @@ jQuery(document).ready(function( $ ) {
 
   // google cloud Chart
   var doughnutData = [{
-      value: 75,
+      value: 70,
       color: "#1abc9c"
     },
     {
-      value: 25,
+      value: 30,
       color: "#ecf0f1"
     }
   ];
@@ -32,11 +32,11 @@ jQuery(document).ready(function( $ ) {
 
   // aws Chart
   var doughnutData = [{
-    value: 75,
+    value: 70,
     color: "#1abc9c"
   },
   {
-    value: 25,
+    value: 30,
     color: "#ecf0f1"
   }
   ];
@@ -56,11 +56,11 @@ jQuery(document).ready(function( $ ) {
 
   // java Chart
   var doughnutData = [{
-    value: 90,
+    value: 85,
     color: "#1abc9c"
   },
   {
-    value: 10,
+    value: 15,
     color: "#ecf0f1"
   }
   ];
@@ -78,6 +78,18 @@ jQuery(document).ready(function( $ ) {
   ];
   var myDoughnut = new Chart(document.getElementById("python").getContext("2d")).Doughnut(doughnutData);
 
+  // javascript Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+
   // spring Chart
   var doughnutData = [{
     value: 85,
@@ -89,4 +101,101 @@ jQuery(document).ready(function( $ ) {
   }
   ];
   var myDoughnut = new Chart(document.getElementById("spring").getContext("2d")).Doughnut(doughnutData);
+
+  // springboot Chart
+  var doughnutData = [{
+    value: 85,
+    color: "#1abc9c"
+  },
+  {
+    value: 15,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("springboot").getContext("2d")).Doughnut(doughnutData);
+
+  // hibernate Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("hibernate").getContext("2d")).Doughnut(doughnutData);
+
+  // mysql Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("mysql").getContext("2d")).Doughnut(doughnutData);
+
+  // postgresql Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("postgresql").getContext("2d")).Doughnut(doughnutData);
+
+  // mongodb Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("mongodb").getContext("2d")).Doughnut(doughnutData);
+
+  // git Chart
+  var doughnutData = [{
+    value: 95,
+    color: "#1abc9c"
+  },
+  {
+    value: 5,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("git").getContext("2d")).Doughnut(doughnutData);
+
+  // docker Chart
+  var doughnutData = [{
+    value: 80,
+    color: "#1abc9c"
+  },
+  {
+    value: 20,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("docker").getContext("2d")).Doughnut(doughnutData);
+
+  // maven Chart
+  var doughnutData = [{
+    value: 90,
+    color: "#1abc9c"
+  },
+  {
+    value: 10,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("maven").getContext("2d")).Doughnut(doughnutData);
+
 });
